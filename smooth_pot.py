00f92bab096771f6c8f7dbf92236ffdf94aa1806
yuphe <yuphe@sandia.gov>
2018-06-21 11:11:25 -0700

    modified smooth_pot.py

--- smooth_pot.py
+++ smooth_pot.py
@@ -4,16 +4,21 @@ from GaussianFun import Gaussian
 parser = argparse.ArgumentParser()
 parser.add_argument("file",help="input potential file")
 parser.add_argument("pair",nargs='+',help='a list of pair potentials')
-parser.add_argument('-s', dest="sigma",help='a width for Gaussian smearing')
-parser.add_argument('-x', dest="xp",help='column number for x')
-parser.add_argument('-y', dest="yp",help='column number for y')
+parser.add_argument('-s', dest="sigma",type=float, help='a width for Gaussian smearing')
+parser.add_argument('-x', dest="xp",type=int,help='column number for x')
+parser.add_argument('-y', dest="yp",type=int,help='column number for y')
 
 args = parser.parse_args()
+
+fname = args.file
+sigma = args.sigma
+xp = args.xp
+yp = args.yp
 
 Npot = 0
 data = []
 nnn = 0
-with open(args.file,'r') as f1:
+with open(fname,'r') as f1:
     for line in f1.readlines():
         nnn = nnn + 1
         if nnn == 4:
@@ -25,8 +30,12 @@ print("Number of potential points:",Npot)
 for i in range(len(data)):
     if len(data[i]) == 1:
         if data[i][0] == args.pair[0]:
-            idx = i+3
-            fdx = idx+Npot
-            temp = Gaussian(data[idx:fdx],args.sigma,args.xp,args.yp)
+            idx = i + 3
+            fdx = idx + Npot
+            temp = [map(float, l) for l in data[idx:fdx]]
+            result = Gaussian(temp, sigma, xp, yp)
 
-print(temp)
+
+with open('smear.dat','w') as f2:
+    for i in range(len(result)):
+        print >>f2,'%.6f \t %.6f' % (result[i][0],result[i][1])
